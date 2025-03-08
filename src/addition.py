2b9c2dcd0ce79bd0bee531865326ff07af9b8c6c
Aravindswamykosana <aarvindkosana@gmail.com>
2025-03-08 13:41:25 +0000

    for python checking

---
+++ src/addition.py
@@ -0,0 +1,14 @@
+# Python program to add two predefined numbers
+
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Predefined numbers
+num1 = 5
+num2 = 7
+
+# Calling the function and displaying the result
+result = add_numbers(num1, num2)
+print("The sum of", num1, "and", num2, "is:", result)
+
